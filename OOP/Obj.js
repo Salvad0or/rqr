94ef7777669de884c0e5f8d8272743a0c857cc3a
- <wavevape@gmail.com>
2023-05-07 20:48:47 +0300

    начало работы с ооп в js

---
+++ OOP/Obj.js
@@ -0,0 +1,21 @@
+const person = Object.create({
+
+},
+
+    {
+        name: {
+            value: 'Alex',
+            enumerable: true, // позволяет перебирать ключ в циклах
+            //writable: true, // позволяет изменять ключ
+            configurable: true // позволяет удалять ключ
+            // Эти штуки называются property-discriptors
+        },
+
+     
+
+    })
+
+    
+
+
+
